47172152d7b5b6f4fd0b119457fbba498403c153
Hailin <hailinfufu@outlook.com>
2019-01-04 10:09:00 +0800

    Site updated: 2019-01-04 10:09:00

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Themes config backup
+mkdir -p ./themes_bak/next
+mkdir -p ./themes_bak/next/layout
+mkdir -p ./themes_bak/next/layout/_macro
+cp -f ./themes/next/_config.yml ./themes_bak/next/_config.yml
+cp -f ./themes/next/layout/_layout.swig ./themes_bak/next/layout/_layout.swig
+cp -f ./themes/next/layout/_macro/post.swig ./themes_bak/next/layout/_macro/post.swig
+
+
 # Generate blog
 hexo clean
 hexo generate
